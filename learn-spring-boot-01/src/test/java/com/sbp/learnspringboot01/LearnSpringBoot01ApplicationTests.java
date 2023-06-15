f93d01feab008d0af2a36421595e454b9b9cd207
ArunDhwaj <sbp.arun.01@gmail.com>
2023-06-15 17:41:03 +0530

    learn-spring-boot-01: Initial code commit.

---
+++ learn-spring-boot-01/src/test/java/com/sbp/learnspringboot01/LearnSpringBoot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbp.learnspringboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBoot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
