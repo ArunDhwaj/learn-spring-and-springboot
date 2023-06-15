4bb486b88aa97b7efa4d583ed5e7839499552e25
ArunDhwaj <sbp.arun.01@gmail.com>
2023-06-15 18:14:50 +0530

    Simple RestAPI added

--- learn-spring-boot-01/src/main/java/com/sbp/learnspringboot01/LearnSpringBoot01Application.java
+++ learn-spring-boot-01/src/main/java/com/sbp/learnspringboot01/LearnSpringBoot01Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnSpringBoot01Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBoot01Application.class, args);
 	}
